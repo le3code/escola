9b9b0eab156b83062703f13b5753525d3f9690a4
le3code <leonardoribeiro.sousa@gmail.com>
2023-05-18 23:10:45 -0300

    primeiro commit projeto escola

---
+++ src/main/java/com/ultima/pessoas/Pessoa.java
@@ -0,0 +1,50 @@
+package com.ultima.pessoas;
+
+public abstract class Pessoa implements PessoaIf {
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+
+    public Pessoa() {} // cintrut padrão
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+
+
+}
+
+
